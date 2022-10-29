ac1561b1e49bfc0e81869f9427528080c01f50bd
GiuseppeZ88 <116847357+GiuseppeZ88@users.noreply.github.com>
2022-10-29 12:00:15 +0200

    Update MKXP.java

--- src/org/ancurio/mkxp/MKXP.java
+++ src/org/ancurio/mkxp/MKXP.java
@@ -1,18 +1,23 @@
 package org.ancurio.mkxp;
 
 import android.content.Context;
-
 import java.io.File;
+import org.libsdl.app.SDLActivity;
 
 public class MKXP
 {
     public static String getLibDir(Context c) {
         String s = c.getApplicationInfo().nativeLibraryDir;
+		String s1 = s;
         if (!s.endsWith(File.separator)) {
-            s += File.separator;
+            s1 = s + File.separator;
         }
-        return s;
+        return s1;
     }
+	
     public static native void loadLibs(String path);
+	
     public static native void unloadLibs();
+	
+	
 }
